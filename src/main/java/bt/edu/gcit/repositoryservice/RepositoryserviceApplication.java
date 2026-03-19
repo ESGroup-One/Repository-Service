193e58e2b468ad68ad39c89d67ea0bfdc5650c32
Lungten <12230028.gcit@rub.edu.bt>
2026-03-19 11:02:00 +0000

    added initial files

---
+++ src/main/java/bt/edu/gcit/repositoryservice/RepositoryserviceApplication.java
@@ -0,0 +1,13 @@
+package bt.edu.gcit.repositoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryserviceApplication.class, args);
+	}
+
+}
